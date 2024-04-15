8287f85519d736bd00bcaa56e2651c2771a1136d
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2024-04-15 17:56:12 -0400

    asd

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,11 +1,5 @@
 
- function getSiteURL(){
-  let url ='http://localhost:3000/';
-  url = url.includes('http') ? url : `https://${url}`;
-  url = url.endsWith('/') ? url : `${url}/`;
-  return url;
-}
 
 export const config = {
-  site: { name: 'Devias Kit', description: '', themeColor: '#090a0b', url: getSiteURL() },
+  site: { name: 'Devias Kit', description: '', themeColor: '#090a0b',  },
 };
